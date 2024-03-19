db12cc963ac353137100f33f443a5b7099d8ae07
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-03-19 16:04:17 +0300

    Print

---
+++ print-function.py
@@ -0,0 +1,3 @@
+#Java'daki gibi, print fonksiyonundaki elemanları ardı ardına eklemek için "+" yerine Python'da "," kullanılır.
+print("{} sayisinin {} sayisi ile çarpiminin sonucu: {}".format(10,12,120))
+#.format fonksiyonu çift tirnaktan sonra gelir. İçine yazılan parametreler sırayla süsülü parantezlerin içine yerleştirilir.
