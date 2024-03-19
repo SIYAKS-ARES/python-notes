9f3e6182317d1007173d1b05a5b8c00c3e2a91df
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-03-19 16:04:40 +0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,9 @@
+'''
+Değişkenlerin ismi sayi ile başlayamaz,
+Değişkenlerin içerisinde arasinda boşluk olamaz,
+Türkçe karakterler kullanilamaz
+'''
+sayi = 20 #Bu şekilde hafızada sayi isminde bir alan oluşturuldu ve bu alanda 20 sayisi tutuluyor.
+print (sayi)
+isim = "SIYAKSARES" #String yapılarında da aynı şekilde değişken oluşturulur, tek fark "" açmanin gerektiğidir.
+print (isim)
