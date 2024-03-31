85d53051bfab1bcdf69f82f17071831a3c9f929c
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-03-31 21:30:11 +0300

    7. Tuplets

--- 7.Tuplets.py
+++ 7.Tuplets.py
@@ -1,6 +1,6 @@
 # Demetler ile liste gibi yapılar oluşturulur tek fark eklenen veriler değiştirilemez.
-# Kullanım alanları: Büyük projerlerde birçok yazılım/programcı çalışıtığı ve birbirlerinin verilerini bozma ihtimalinin önüne geçmek,
-# çözmesi en zor olan mantıksal hataları minimuma indirmek için kullanılır.
+''' Kullanım alanları: Büyük projerlerde birçok yazılım/programcı çalışıtığı ve birbirlerinin verilerini bozma ihtimalinin önüne geçmek,
+çözmesi en zor olan mantıksal hataları minimuma indirmek için kullanılır.'''
 
 sabitler = (3.12, 3.13, 3.14, 3.15) # Listede "[3]" kullanılırken tuple'larda "()" kullanılır.
 print (sabitler)
