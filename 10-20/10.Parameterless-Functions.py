c3e6cc5fc46ea63946eed758f198b97997db2c27
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-03-31 23:16:20 +0300

    10. Parameterless Functions

---
+++ 10-20/10.Parameterless-Functions.py
@@ -0,0 +1,9 @@
+# isimYaz() "isimYaz" is not definedPylancereportUndefinedVariable
+
+def isimYaz(): # def anahtar kelimesi ile fonksiyonlar yazılabilir.
+    print("SIYAKSARES")
+    
+isimYaz()
+# Parantezi açıp kapatmak önemli bu bir fonksiyon olduğunu gösterir.
+# Fonksiyon yazıldıktan sonra çağırılabilir.
+# Parametresiz fonskiyonlar parantez içine parametre almayan fonskiyonları ifade eder.
