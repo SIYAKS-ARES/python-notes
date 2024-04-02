c691daa066a105d40786112d4ae3a604cbc11012
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-04-02 22:20:52 +0300

    10. Parameterless Functions

--- 10-20/10.Parameterless-Functions.py
+++ 10-20/10.Parameterless-Functions.py
@@ -1,5 +1,9 @@
 # isimYaz() "isimYaz" is not definedPylancereportUndefinedVariable
 
+'''
+Python'da bir sınıf içindeki bir yöntemi (method) çağırırken, bu yöntemin bir örnek üzerinde çalışacağını belirtmek için
+self anahtar kelimesini kullanırız. self, o anki örneği temsil eder ve sınıf içindeki diğer özelliklere ve yöntemlere erişim sağlar. '''
+
 def isimYaz(): # def anahtar kelimesi ile fonksiyonlar yazılabilir.
     print("SIYAKSARES")
     
