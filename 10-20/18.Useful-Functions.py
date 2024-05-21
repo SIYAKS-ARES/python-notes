95c9bf11aaad193ad1750bdb4b996f968cde3ca9
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-05-21 16:25:13 +0300

    18. Useful Functions

--- 10-20/18.Useful-Functions.py
+++ 10-20/18.Useful-Functions.py
@@ -14,7 +14,7 @@ t = dict(zip(xZip, yZip)) # Dönüştürmede çoğunlukla liste olarak değil, k
 print(z)
 print(t)
 
-################################################################
+###############################################################
 
 # MAP FUNCTIONS
 
@@ -81,3 +81,159 @@ hataliSifreler = list(filter(lambda n: n.__contains__(".") or n.__contains__("?"
 print(m)
 print(n)
 print(hataliSifreler)
+
+#############################################################
+
+# REDUCE FUNCTION
+
+'''REDUCE fonksiyonu bir tane fonksiyon ve iterable edilebilen veri kümesi alır ardından sadece tek bir sonuç verir.
+Bunu elemanları eksilterek, kendinde toplayarak yapar.
+'''
+
+list = [12,46,54,62,7634,8324,966,810]
+
+def topla(sayi1, sayi2):
+    return sayi1 + sayi2
+
+print(reduce(topla, list))
+# sayi1 ile sayi2'yi toplar ardından sonucu sayi1'e atar ve sıradaki sayıyı sayi2'ye atayarak devam eder. Ve son sonucu verir.
+
+print(reduce(lambda n,m: n + m, [12,46,54,62,7634,8324,966,810])) # Lamda kullanılarak yapılan hali.
+
+#############################################################
+
+
+# REDUCE FUNCTION
+
+'''REDUCE fonksiyonu, verilen iterable edilebilen veri kümesinin elemanlarını birbirinden
+bir sonraki elemanla toplayarak bir sonuç elde eder.
+'''
+
+from functools import reduce
+
+xReduce = [1, 2, 4, 8, 12, 7, 5]
+
+
+def topla(sayi1, sayi2):
+    return sayi1 + sayi2
+
+def carp(sayi1, sayi2):
+    return sayi1 * sayi2
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+# reduce fonksiyonu, verilen iterable edilebilen veri kümesinin elemanlarını birbirinden bir sonraki elemanla toplayarak bir sonuç elde eder.
+
+
+print(reduce(topla, xReduce))
+print(reduce(carp, xReduce))
+print(reduce(ciftMi, xReduce))
+print(reduce(kareAl, xReduce))
+
+#############################################################
+
+
+# ALL FUNCTION
+
+'''ALL fonksiyonu, verilen iterable edilebilen veri kümesinin tüm elemanlarına true döndüren bir fonksiyonun
+sonucunu döndürür.
+'''
+
+xAll = [1, 2, 4, 8, 12, 7, 5]
+yAll = [3, 6, 9, 12, 15, 18, 21]
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+print(all(map(ciftMi, xAll)))
+print(all(map(kareAl, xAll)))
+
+#############################################################
+
+
+# ANY FUNCTION
+
+'''ANY fonksiyonu, verilen iterable edilebilen veri kümesinin herhangi bir elemanına true döndüren bir fonksiyonun
+sonucunu döndürür.
+'''
+
+xAny = [1, 2, 4, 8, 12, 7, 5]
+yAny = [3, 6, 9, 12, 15, 18, 21]
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+print(any(map(ciftMi, xAny)))
+print(any(map(kareAl, xAny)))
+
+#############################################################
+
+
+# SUM FUNCTION
+
+'''SUM fonksiyonu, verilen iterable edilebilen veri kümesinin elemanlarının toplamını döndürür.
+'''
+
+xSum = [1, 2, 4, 8, 12, 7, 5]
+ySum = [3, 6, 9, 12, 15, 18, 21]
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+print(sum(map(ciftMi, xSum)))
+print(sum(map(kareAl, xSum)))
+
+#############################################################
+
+
+# MIN FUNCTION
+
+'''MIN fonksiyonu, verilen iterable edilebilen veri kümesinin en küçük elemanını döndürür.
+'''
+
+xMin = [1, 2, 4, 8, 12, 7, 5]
+yMin = [3, 6, 9, 12, 15, 18, 21]
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+print(min(map(ciftMi, xMin)))
+print(min(map(kareAl, xMin)))
+
+#############################################################
+
+
+# MAX FUNCTION
+
+'''MAX fonksiyonu, verilen iterable edilebilen veri kümesinin en büyük elemanını döndürür.
+'''
+
+xMax = [1, 2, 4, 8, 12, 7, 5]
+yMax = [3, 6, 9, 12, 15, 18, 21]
+
+def ciftMi(sayi):
+    return True if  sayi % 2 == 0 else False
+
+def kareAl(sayi):
+    return sayi ** 2
+
+print(max(map(ciftMi, xMax)))
+print(max(map(kareAl, xMax)))
+
+#############################################################
