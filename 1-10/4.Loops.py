178428575e238d7760efb2f564914b4dede93063
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-05-21 16:52:37 +0300

    Loops - Update

--- 1-10/4.Loops.py
+++ 1-10/4.Loops.py
@@ -1,6 +1,6 @@
 for i in range(10): # "range()" fonksiyonunda içine yazılan sayıya kadar gider, o sayıyı dahil etmez.
     print(i)
-    
+
 #Eğer "range(a, b)" şeklinde verilirse a'dan başlayıp, b'ye kadar sayar.
 #Eğer "range(a, b, c)" şeklinde verilirse a'dan başlayıp, b'ye kadar c'şey sayar.
 print("Ara")
