bbc515df46770ae9fdc28b49662f8be54868acad
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-09 20:29:11 +0300

    1. Print & Veriables

--- 1-10/1.Print-and-Veriables.py
+++ 1-10/1.Print-and-Veriables.py
@@ -1,3 +1,7 @@
+#Java'daki gibi, print fonksiyonundaki elemanları ardı ardına eklemek için "+" yerine Python'da "," kullanılır.
+print("{} sayisinin {} sayisi ile çarpiminin sonucu: {}".format(10,12,120))
+#".format" fonksiyonu çift tirnaktan sonra gelir. İçine yazılan parametreler sırayla süsülü parantezlerin içine yerleştirilir.
+
 '''
 Değişkenlerin ismi sayi ile başlayamaz,
 Değişkenlerin içerisinde arasinda boşluk olamaz,
