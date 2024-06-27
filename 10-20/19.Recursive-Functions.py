9db2a69840fb64539d4843e439140727430ccb45
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 20:05:12 +0300

    19. Recursive Functions

--- 10-20/19.Recursive-Functions.py
+++ 10-20/19.Recursive-Functions.py
@@ -1,10 +1,12 @@
-'''Belirli kurallara göre, devamlı kendi kendini çağıran fonksiyonlara Özyinemeli fonskiyonlar denir.
+'''
+Belirli kurallara göre, devamlı kendi kendini çağıran fonksiyonlara Özyinemeli fonskiyonlar denir.
 Recursive fonksiyonların tüm işlemlerini döngüler yardımıyla da yapabiliriz.
 Recursive fonksiyonlar çok fazla bellek kullanır ve döngülere göre daha yavaştırlar.
 Zamanı da çok iyi yönetemezler.
 Kullanım amacı programlama yaparken daha okunabilir ve temiz bir kod yazmaya çalışmaktır. Genel olarak dögüler yapı olarak daha
 basit olsa da bazı durumlarda çok karmaşıklaşabiliyor. Eksta olarak Python programalama dilinde en fazla 1000 adet yineleme yapılabiliyor.
-Bu sayı arttırılmak istendiğinde Python'un özel dosyalarını kullanmanız gerekir.'''
+Bu sayı arttırılmak istendiğinde Python'un özel dosyalarını kullanmanız gerekir.
+'''
 
 # Recursive'ler stack/yığın yapısıyla çalışırlar.
 
@@ -22,7 +24,8 @@ def listSum(list):
     else:
         return list[0] + listSum(list[1:])
 
-'''This line performs the recursive step. It returns the sum of the first element of the list (list[0]) and
+'''
+This line performs the recursive step. It returns the sum of the first element of the list (list[0]) and
 the result of calling the listSum function recursively with the remaining elements of the list (list[1:]).
 The list[1:] syntax creates a new list that contains all elements of the original list except the first one.
 This new list is passed as an argument to the recursive call of listSum.
@@ -30,7 +33,8 @@ The recursion continues until the base case (len(list) == 0) is reached, at whic
 and the function starts returning the sum of the elements.
 In summary, this function calculates the sum of all elements in a list using recursion. It does this by breaking
 down the problem into smaller sub-problems (summing the remaining elements of the list) until the base case (an empty list) is reached.
-Then, it combines the results of the sub-problems to get the final sum.'''
+Then, it combines the results of the sub-problems to get the final sum.
+'''
 
 print(listSum([1, 2, 3, 4, 5]))
 
