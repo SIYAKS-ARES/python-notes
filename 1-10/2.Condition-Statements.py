be9c62df0c45552c20c77a98babae2e0b5354e7b
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 19:31:18 +0300

    2. Condition Statements

--- 1-10/2.Condition-Statements.py
+++ 1-10/2.Condition-Statements.py
@@ -2,19 +2,19 @@ sayi = 10
 
 if(sayi == 0):# "=" atama operatörüyken (Ps. işaretin sağındaki değeri alıp soldakine atama), "==" karşılaştırma operatörüdür.
     print ("Sayi 0'a eşittir.")
-    #Python'da çıkıntı kavrami vardir. Koşulun bir alt çıkıntısı içeriye girildiği zaman yapılacak kodları barındırır.
+    # Python'da çıkıntı yöntemi kullanılır, koşulun bir alt çıkıntısı içeriye girildiği zaman yapılacak kodları barındırır.
 elif(sayi < 0):
     print("Sayi sifirdan kucukutur")
 elif(sayi > 0):
     print("Sayi sifirdan buyuktur")
 else:
-    print ("Sayi 0'a esit degildir.")
+    print ("Girilen sayi rasyonel sayi değildir")
 
-###############Condition Statement Examples
+################################## Condition Statement Examples
 
-#İlk örnek doğum tarihine göre ehliyet alıp alamayacağını gösteren yapı
+# İlk örnek; doğum tarihine göre ehliyet alıp alamayacağını gösteren yapı
 
-dogumTarihi = 2000  #Bu şekilde ilk harf küçük, sonraki kelimenin harfi büyük olan yapılara "Camel Case" denir.
+dogumTarihi = 2000  # Bu şekilde ilk harf küçük, sonraki kelimenin harfi büyük olan yapılara "Camel Case" denir.
 tarih = 2024
 yas = tarih - dogumTarihi
 if(yas >= 18):
@@ -22,29 +22,29 @@ if(yas >= 18):
 else:
     print(yas, "Oldugu ve 18 yasindan kucuk oldugunuz icin ehliyet alamazsiniz!")
 
-#İkinci örnek sisteme giriş yapma yapısı
+# İkinci örnek sisteme giriş yapma yapısı
 
-#kullaniciAdi = "SIYAKSARES" #Tırnak işareti unutulmamalıdır.
-#kullaniciSifresi = "1234" #Burada da matematiksel bir işlem yapılmayacağı için tırnak içine alınır.
+# kullaniciAdi = "SIYAKSARES" #Tırnak işareti unutulmamalıdır.
+# kullaniciSifresi = "1234" #Burada da matematiksel bir işlem yapılmayacağı için tırnak içine alınır.
 
-#Ayrıca kullanıcıdan da giriş bilgileri istenebilir.
+# Ayrıca kullanıcıdan da giriş bilgileri istenebilir.
 kullaniciAdi = input("Kullanici adini giriniz: ")
 kullaniciSifresi = input("Kullanici sifresini giriniz: ")
 
-if(kullaniciAdi == "SIYAKSARES" and kullaniciSifresi=="1234"):
+if(kullaniciAdi == "SIYAKSARES" and kullaniciSifresi == "1234"):
     print("Girilen bilgiler dogru, giris yapiliyor!")
 else:
-    print("Girilen bilgiler hatali, cikis yapiliyor!")
+    print("Girilen bilgiler hatalidir, cikis yapiliyor!")
 
-###############Kısa Yazılan Koşul İfadeleri
+#################### Kısa Yazılan Koşul İfadeleri
 import math
 
-sayi2 = int(input("Lutfen bir sayi giriniz: ")) #Gelen String değerini integer değere çevirmek için başına "int(" getirilir.
+sayi2 = int(input("Lutfen bir sayi giriniz: ")) # Gelen String değerini integer değere çevirmek için başına "int(" getirilir.
 
-filtreliSayi = sayi2 if sayi2 >= 0 else math.fabs(sayi2) #"math.fabs()" fonksiyonu ondalıklı değerle mutlak değerini alır.
+filtreliSayi = sayi2 if sayi2 >= 0 else math.fabs(sayi2) # "math.fabs()" fonksiyonu ondalıklı değerle mutlak değerini alır.
 print("Sayiniz: " , filtreliSayi)
 
 sayi3 = int(input("Lutfen bir sayi giriniz: "))
-islenmisDeger = sayi3 if sayi3 == 10 else sayi3*2 if sayi3 > 10 else sayi3/2 #Burada da koşulun gerçekleşmeme koşulunu koşula bağlamış olduk.
-#Sayı 10 iken 10'u, 12 iken 24'ü, 8 iken 4'ü verir.
+islenmisDeger = sayi3 if sayi3 == 10 else sayi3*2 if sayi3 > 10 else sayi3/2 # Burada da koşulun gerçekleşmeme koşulunu koşula bağlamış olduk.
+# Sayı 10 iken 10'u, 12 iken 24'ü, 8 iken 4'ü verir.
 print("Yeni sayiniz: " , islenmisDeger)
