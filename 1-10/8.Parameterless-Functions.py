a4bce595c2b440a1e19ebc7bd29cb0bae2d9a4a3
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 19:47:57 +0300

    8. Parameterless Functions

--- 1-10/8.Parameterless-Functions.py
+++ 1-10/8.Parameterless-Functions.py
@@ -2,12 +2,13 @@
 
 '''
 Python'da bir sınıf içindeki bir yöntemi (method) çağırırken, bu yöntemin bir örnek üzerinde çalışacağını belirtmek için
-self anahtar kelimesini kullanırız. self, o anki örneği temsil eder ve sınıf içindeki diğer özelliklere ve yöntemlere erişim sağlar. '''
+self anahtar kelimesini kullanırız."self", o anki örneği temsil eder ve sınıf içindeki diğer özelliklere ve yöntemlere erişim sağlar.
+'''
 
 def isimYaz(): # def anahtar kelimesi ile fonksiyonlar yazılabilir.
     print("SIYAKSARES")
 
 isimYaz()
-# Parantezi açıp kapatmak önemli bu bir fonksiyon olduğunu gösterir.
+# Parantezi açıp kapatmak önemli, bu bir fonksiyon olduğunu gösterir.
 # Fonksiyon yazıldıktan sonra çağırılabilir.
-# Parametresiz fonskiyonlar parantez içine parametre almayan fonskiyonları ifade eder.
+# Parametresiz fonskiyonlar, parantez içine parametre almayan fonskiyonları ifade eder.
