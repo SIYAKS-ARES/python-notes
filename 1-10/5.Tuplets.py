855d759fc19d7a09a96adb1c4639539fae94d4a5
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 19:41:16 +0300

    5. Tuplets

--- 1-10/5.Tuplets.py
+++ 1-10/5.Tuplets.py
@@ -1,8 +1,8 @@
-# Demetler ile liste gibi yapılar oluşturulur tek fark eklenen veriler değiştirilemez.
+# Demetler/Tuple'lar ile liste gibi yapılar oluşturulur tek fark eklenen veriler değiştirilemez.
 ''' Kullanım alanları: Büyük projerlerde birçok yazılım/programcı çalışıtığı ve birbirlerinin verilerini bozma ihtimalinin önüne geçmek,
 çözmesi en zor olan mantıksal hataları minimuma indirmek için kullanılır.'''
 
-sabitler = (3.12, 3.13, 3.14, 3.15) # Listede "[3]" kullanılırken tuple'larda "()" kullanılır.
+sabitler = (3.12, 3.13, 3.14, 3.15) # Listede "[]" kullanılırken tuple'larda "()" kullanılır.
 print (sabitler)
 print (type(sabitler)) # "type()" fonksiyonu ile sabitlerin türü yazdırılabilir. <class "tuple">
 print (sabitler.count(3.14)) # ".count" fonksiyonu ile kaç defa geçtiğini görebiliriz.
