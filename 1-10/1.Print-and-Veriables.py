b1281c0d6d4659d3ab66c2216328b547fd52f559
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 19:26:16 +0300

    1. Print & Veriables

--- 1-10/1.Print-and-Veriables.py
+++ 1-10/1.Print-and-Veriables.py
@@ -1,19 +1,18 @@
-#Java'daki gibi, print fonksiyonundaki elemanları ardı ardına eklemek için "+" yerine Python'da "," kullanılır.
+# Java'daki gibi, print fonksiyonundaki elemanları ardı ardına eklemek için "+" yerine Python'da "," kullanılır.
 print("{} sayisinin {} sayisi ile çarpiminin sonucu: {}".format(10,12,120))
-#".format" fonksiyonu çift tirnaktan sonra gelir. İçine yazılan parametreler sırayla süsülü parantezlerin içine yerleştirilir.
+# ".format" fonksiyonu çift tirnaktan sonra gelir. İçine yazılan parametreler sırayla süsülü parantezlerin içine yerleştirilir.
 
 '''
 Değişkenlerin ismi sayi ile başlayamaz,
 Değişkenlerin içerisinde arasinda boşluk olamaz,
 Türkçe karakterler kullanilamaz
 '''
-sayi = 20 #Bu şekilde hafızada sayi isminde bir alan oluşturuldu ve bu alanda 20 sayisi tutuluyor.
+sayi = 20 # Bu şekilde hafızada sayi isminde bir alan oluşturuldu ve bu alanda 20 sayisi tutulur.
 print (sayi)
-isim = "SIYAKSARES" #String yapılarında da aynı şekilde değişken oluşturulur, tek fark "" açmanin gerektiğidir.
+isim = "SIYAKSARES" # String yapılarında da aynı şekilde değişken oluşturulur, tek fark "" açmanin gerekmesidir.
 print (isim)
 
-
-#3.VAT-Sale-Example
+# 3.VAT-Sale-Example
 fiyat = 100
 kdv = 18
 kdvTutari = fiyat * kdv / 100
