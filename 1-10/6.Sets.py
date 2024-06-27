030e249c19ec02e09ef7f301dc9979e78ed4185c
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-27 19:42:27 +0300

    6. Sets

--- 1-10/6.Sets.py
+++ 1-10/6.Sets.py
@@ -1,8 +1,8 @@
-#Bir küme içerisinde aynı elemandan yalnızca bir tane olabilir.
-küme = {1,2,3,4,5,6,7,8,9,1,2,3,4,5} #Süslü parantez kullanılır.
+# Bir küme içerisinde aynı elemandan yalnızca bir tane olabilir.
+küme = {1,2,3,4,5,6,7,8,9,1,2,3,4,5} # Süslü parantez kullanılır.
 print (type(küme))
-#Set'ler indexlenemezler.
+# Set'ler indexlenemezler.
 print (len(küme)) #"len()" fonksiyonu kümenin sayısını yazdırır.
-# küme."***" ile çıkan fonksiyonar yapılabilir, bunlar; kaldırma, fark, kopyala, kesişim gibi normal küme mantığı ile yapılabilecek işlemlerdir.
+# 'küme."***"' ile çıkan fonksiyonar yapılabilir, bunlar; kaldırma, fark, kopyala, kesişim gibi normal küme mantığı ile yapılabilecek işlemlerdir.
 numaralar = {1,2,3}
 print (küme.difference(numaralar))
