cadf9c366a14e10d6de921849fe82188c31b45b2
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-07-02 20:24:52 +0300

    11. Lambda Fucntions

--- 10-20/11.Lambda-Functions.py
+++ 10-20/11.Lambda-Functions.py
@@ -1,17 +1,18 @@
 '''
-Normal fonksiyonların tek satırda yazılmasıdır. Parametre olarak fonksiyon alan fonksiyonlara fonksiyon göndermek için
-kullanılan fonksiyonlara denir. Genellikle kodun daha okunabilir olması için kullanılır. Tek yerde kullanılacak olan
-fonksiyonlarda tercih edilir. Genel olarak isim verilmez.
+Lambda fonksiyonları, basit fonksiyonların tek satırda yazılmasıdır. Parametre olarak başka fonksiyonlar alabilen ve bu fonksiyonlara
+başka fonksiyonlar göndermek için kullanılan fonksiyonlardır. Genellikle kodun daha okunabilir olması için tercih edilirler.
+Lambda fonksiyonları genellikle tek bir yerde kullanılacak olan fonksiyonlar için uygundur ve bu nedenle isim verilmez.
+İşlevselliği artırmak ve kodun daha kısa ve öz olmasını sağlamak amacıyla kullanılırlar.
 '''
 
 karesiniAl = lambda x: x*x
 
 carp = lambda x, y: x*y
 
-print(karesiniAl(karesiniAl(4))) # Tekrar da çağırılabiliyor.
+print(karesiniAl(karesiniAl(4))) # Tekrar da çağırılabilirer.
 
-print(carp(carp(3, 4), karesiniAl(4))) # Çağırılan fonksiyonları iki paremetre olarak da çağırabiliyoruz.,
+print(carp(carp(3, 4), karesiniAl(4))) # Çağırılan fonksiyonları iki paremetre olarak da çağırabilirler.
 
-print((lambda x, y: x/y)(2,4)) # İsim vermeden tek satırda bu şekilde kullanılır.
+print((lambda x, y: x/y)(2,4)) # İsim vermeden tek satırda bu şekilde kullanılırlar.
 
-print((lambda x: x%10==0)(30)) # Boolean değerler de döndürebiliyor.
+print((lambda x: x%10==0)(30)) # Boolean değerler de döndürebilirler.
